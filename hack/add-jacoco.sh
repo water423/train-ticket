8d97b28157d63a970e710b0347c4ae1f44b03291
程千骞 <1617095973@qq.com>
2022-06-13 07:37:44 +0800

    jacoco sh

--- hack/add-jacoco.sh
+++ hack/add-jacoco.sh
@@ -8,7 +8,7 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "add-jacoco ${dir}"
-            cp /home/jacoco-0.8.2/lib/jacocoagent.jar "${dir}"/target/
+            cp jacocoagent.jar "${dir}"/target/
         fi
     fi
 done
