f8f9583044dd277db6598e06fd60edef75d65bfb
root <root@localhost.localdomain>
2022-06-13 12:13:52 +0800

    jacocoagent.jar

--- hack/add-jacoco.sh
+++ hack/add-jacoco.sh
@@ -6,11 +6,11 @@ echo "add jacoco.jar"
 echo
 for dir in ts-*; do
     if [[ -d $dir ]]; then
-      echo "add-jacoco ${dir}"
-      cp jacocoagent.jar "${dir}"/
-#        if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
+#      echo "add-jacoco ${dir}"
+ #     cp jacocoagent.jar "${dir}"/
+        if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
 #            echo "add-jacoco ${dir}"
-#            cp jacocoagent.jar "${dir}"/
-#        fi
+            cp jacocoagent.jar "${dir}"/
+        fi
     fi
 done
